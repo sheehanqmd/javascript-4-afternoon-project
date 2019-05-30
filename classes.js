6ab987a2407c37176a189379bdc54a09ef211a27
donna <sheehanqmd@gmail.com>
2019-05-29 22:52:22 -0500

    complete

--- classes.js
+++ classes.js
@@ -30,6 +30,20 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+      makeWidget(){ 
+      return this.first_name + " " + this.last_name +  " Widget"}
+    }
+  
+  
+
+  
 
 
 ////////// PROBLEM 2 //////////
@@ -48,7 +62,20 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor (first_name, last_name, email, age, reports) { 
+  super (first_name, last_name, email, age) 
+  this.reports = []
 
+}
+hire(employee){
+  this.reports.push(employee)
+
+}
+fire(index){
+  this.reports.splice(index, 1)
+}
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,6 +99,48 @@
 */
 
 //Code Here
+
+class ProgressiveManager extends Manager {
+  constructor (first_name, last_name, email, age, reports, title){ 
+  super (first_name, last_name, email, age, reports, title)
+  this.title = 'Not a manager';
+  this.bonus = 0;
+  }
+
+
+  hire(employee){
+   this.reports.push(employee)
+   if(this.reports.length === 0) {
+      this.title = 'Not a manager';
+    }
+    else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager';
+    }
+    else if (this.reports.length >= 4 && this.reports.length <= 10) { 
+      this.title = 'Mostly Manager';
+    }
+    else if (this.reports.length >= 11 && this.reports.length <= 50) { 
+      this.title = 'Manager';
+    }
+    else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus';
+    }
+    else if (this.reports.length >= 101){
+      this.title = 'Bestest Manager';
+    }
+  }
+     
+
+  fire(index){ 
+      this.reports.splice(index, 1);
+    this.bonus += 100;
+  }
+  }
+
+
+
+ 
+
 
 
 
@@ -99,5 +168,4 @@
 */
 
 //Code Here
-
 
--- context.js
+++ context.js
@@ -20,6 +20,18 @@
   
 
 //Code Here
+let user = {
+  username: "",
+  email: "",
+  getUsername: function currentUser(){
+    return (
+      this.username
+    )
+  }
+}
+
+
+
 
 
 
@@ -43,6 +55,10 @@ class Animal {
 
 //Code Here
 
+let animal1 = new Animal("Bob", "Tiger", "cheese");
+console.log(animal1);
+
+
 
 
 ////////// PROBLEM 3 //////////
@@ -64,6 +80,12 @@ let who = {
 //Code Here
 
 
+let whoSaysHi = sayHi.bind(who);
+
+ 
+whoSaysHi();
+
+
 
 ////////// PROBLEM 4 //////////
 
@@ -76,7 +98,8 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = this;
+
 
 let product = {
   name: 'snake plant',
@@ -87,7 +110,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +121,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -116,4 +139,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1.whatIsThis()
